b077d3cbe9b8f10a49cf8019b9005550aba8ccb7
Marina Mosti <marina@mosti.com.mx>
2020-05-26 09:40:57 -0500

    Create SalutationName component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SalutationName
+      v-model:salutation="form.salutation"
+      v-model:name="form.name"
+    />
+
+    <pre>{{ form }}</pre>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue'
+import SalutationName from './components/SalutationName'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SalutationName
+  },
+  setup () {
+    const form = reactive({
+      salutation: '',
+      name: ''
+    })
+
+    return {
+      form
+    }
   }
 }
 </script>
@@ -24,5 +39,96 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+pre {
+  display: block;
+  width: 50%;
+  text-align: left;
+}
+
+button,
+label,
+input,
+optgroup,
+select,
+textarea {
+  display: inline-flex;
+  font-family: "Open sans", sans-serif;
+  font-size: 100%;
+  line-height: 1.15;
+  margin: 0;
+}
+input,
+textarea {
+  box-sizing: border-box;
+  border: solid 1px rgba(0, 0, 0, 0.4);
+}
+textarea {
+  width: 100%;
+  overflow: auto;
+  font-size: 20px;
+}
+[type="checkbox"],
+[type="radio"] {
+  box-sizing: border-box;
+  padding: 0;
+  margin-right: 0.5rem;
+}
+[type="number"]::-webkit-inner-spin-button,
+[type="number"]::-webkit-outer-spin-button {
+  height: auto;
+}
+[type="search"] {
+  -webkit-appearance: textfield;
+  outline-offset: -2px;
+}
+[type="search"]::-webkit-search-decoration {
+  -webkit-appearance: none;
+}
+[type="text"],
+[type="number"],
+[type="search"],
+[type="password"] {
+  height: 52px;
+  width: 100%;
+  padding: 0 10px;
+  font-size: 20px;
+}
+[type="text"]:focus,
+[type="number"]:focus,
+[type="search"]:focus,
+[type="password"]:focus {
+  border-color: #39b982;
+}
+select {
+  height: 52px;
+  padding: 0 24px 0 10px;
+  vertical-align: middle;
+  background: #fff
+    url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 4 5'%3E%3Cpath fill='%23343a40' d='M2 0L0 2h4zm0 5L0 3h4z'/%3E%3C/svg%3E")
+    no-repeat right 12px center;
+  background-size: 8px 10px;
+  border: solid 1px rgba(0, 0, 0, 0.4);
+  border-radius: 0;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+select:focus {
+  border-color: #39b982;
+  outline: 0;
+}
+select:focus::ms-value {
+  color: #000;
+  background: #fff;
+}
+select::ms-expand {
+  opacity: 0;
 }
 </style>
---
+++ src/components/SalutationName.vue
@@ -0,0 +1,59 @@
+<template>
+  <select name="salutation" @change="$emit('update:salutation', $event.target.value)">
+    <option value="">-</option>
+    <option
+      v-for="slt of salutations"
+      :value="slt"
+      :key="slt"
+      :selected="salutation === slt"
+    >
+      {{ slt }}
+    </option>
+  </select>
+
+  <input
+    :value="name"
+    @input="$emit('update:name', $event.target.value)"
+    type="text"
+    name="name"
+  />
+</template>
+
+<script>
+const salutations = [
+  'Ms.',
+  'Mrs.',
+  'Miss',
+  'Mx.',
+  'Dr.'
+]
+
+export default {
+  emits: ['update:salutation', 'update:name'],
+  props: {
+    salutation: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: ''
+    }
+  },
+  setup () {
+    return {
+      salutations
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+select {
+  margin-right: 0.5rem;
+}
+
+input {
+  width: 50%;
+}
+</style>
