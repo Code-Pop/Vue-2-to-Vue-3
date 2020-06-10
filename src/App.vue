e208d1e70828f09167af5105d70b751416fa4606
Marina Mosti <marina@mosti.com.mx>
2020-06-10 16:25:49 -0500

    Revert to l2 start

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-    <SalutationName
-      v-model:salutation.capitalize="form.salutation"
-      v-model:name.capitalize.reverse="form.name"
-    />
 
     <pre>{{ form }}</pre>
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
-import SalutationName from './components/SalutationName'
-
 export default {
   name: 'App',
   components: {
-    SalutationName
+
   },
   setup () {
-    const form = reactive({
-      salutation: '',
-      name: ''
-    })
 
-    return {
-      form
-    }
   }
 }
 </script>
--- src/components/SalutationName.vue
+++ src/components/SalutationName.vue
@@ -1,26 +1,6 @@
 <template>
   <div>
-    <select
-      name="salutation"
-      @change="updateSalutation"
-    >
-      <option value="">-</option>
-      <option
-        v-for="item of salutations"
-        :value="item"
-        :key="item"
-        :selected="salutation === item"
-      >
-        {{ item }}
-      </option>
-    </select>
 
-    <input
-      :value="name"
-      @input="updateName"
-      type="text"
-      name="name"
-    />
   </div>
 </template>
 
@@ -35,49 +15,10 @@ const salutations = [
 
 export default {
   props: {
-    salutation: {
-      type: String,
-      default: ''
-    },
-    salutationModifiers: {
-      default: () => ({})
-    },
-    name: {
-      type: String,
-      default: ''
-    },
-    nameModifiers: {
-      default: () => ({})
-    }
+
   },
-  setup (props, { emit }) {
-    const updateSalutation = event => {
-      let val = event.target.value
-      if (props.salutationModifiers.capitalize) {
-        val = val.toUpperCase()
-      }
+  setup () {
 
-      emit('update:salutation', val)
-    }
-
-    const updateName = event => {
-      let val = event.target.value
-      if (props.nameModifiers.capitalize) {
-        val = val.charAt(0).toUpperCase() + val.slice(1)
-      }
-
-      if (props.nameModifiers.reverse) {
-        val = val.split('').reverse().join('')
-      }
-
-      emit('update:name', val)
-    }
-
-    return {
-      salutations,
-      updateSalutation,
-      updateName
-    }
   }
 }
 </script>
